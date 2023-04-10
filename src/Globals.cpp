34118ab6e65aae82e049dd4781f5300bb73b934b
Stephan Mühl <31169771+Blueforcer@users.noreply.github.com>
2023-04-10 18:29:12 +0200

    increase version

--- src/Globals.cpp
+++ src/Globals.cpp
@@ -156,7 +156,7 @@ IPAddress gateway;
 IPAddress subnet;
 IPAddress primaryDNS;
 IPAddress secondaryDNS;
-const char *VERSION = "0.48";
+const char *VERSION = "0.49";
 String MQTT_HOST = "";
 uint16_t MQTT_PORT = 1883;
 String MQTT_USER;
