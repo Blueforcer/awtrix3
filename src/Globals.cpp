838bf3b276f43cfc87e57805bbf58a62aaeb0346
Stephan Mühl <31169771+Blueforcer@users.noreply.github.com>
2023-04-30 16:38:28 +0200

    change flashername

--- src/Globals.cpp
+++ src/Globals.cpp
@@ -214,7 +214,7 @@ IPAddress gateway;
 IPAddress subnet;
 IPAddress primaryDNS;
 IPAddress secondaryDNS;
-const char *VERSION = "0.60";
+const char *VERSION = "0.59";
 String MQTT_HOST = "";
 uint16_t MQTT_PORT = 1883;
 String MQTT_USER;
