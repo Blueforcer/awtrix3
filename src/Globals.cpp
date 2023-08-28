02adb6d239c107130dc048d94f1c76d3b12c8c4b
Stephan Mühl <31169771+Blueforcer@users.noreply.github.com>
2023-08-28 23:33:11 +0200

    v0.82

--- src/Globals.cpp
+++ src/Globals.cpp
@@ -286,7 +286,7 @@ IPAddress gateway;
 IPAddress subnet;
 IPAddress primaryDNS;
 IPAddress secondaryDNS;
-const char *VERSION = "0.81";
+const char *VERSION = "0.82";
 
 String MQTT_HOST = "";
 uint16_t MQTT_PORT = 1883;
